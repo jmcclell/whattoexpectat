5c889ec0a9134ad811332805f123204421b9626d
Jason McClellan <jason@jasonmcclellan.net>
2014-03-04 01:08:39 -0500

    WIP

--- app/scripts/controllers/review.js
+++ app/scripts/controllers/review.js
@@ -9,36 +9,37 @@ angular.module('whattoexpectatApp')
 
       var slug = $routeParams.slug;
       var gref = $location.search().gref;
-
       var mapDefer = $q.defer();
 
       $scope.searchContext = SearchService;
+
       $scope.review = {
               placeDetail: null,
               place: PlaceService.getPlaceBySlug(slug),
               reviews: [],
               recommendationModifier: 0
             };
-      
+
       if (gref) {
         initializeWithGref();
       } else {
-        var grefRef = $scope.review.place.$child('gref');
-        grefRef.$on('loaded', function(data) {
-          gref = data;
-          if(!gref) {
-            console.log('no gref');
-            $location.url('/?q=' + slug.split('-').join(' '));         //
-          } else {
+        $scope.review.place.$on('loaded', function(snapshot) {
+          console.log('Place loaded');
+          if (snapshot && snapshot.gref) {
+            console.log('gref came from stored Place');
+            gref = snapshot.gref;
             initializeWithGref();
+          } else {
+            console.log('Can\'t find a gref');
+            $location.url('/?q=' + slug.split('-').join(' '));
           }
-        });       
+        });
       }
-      
+
       function initializeWithGref() {
         SearchService.getPlaceDetailByGRef(gref)
-          .then(function(placeDetail) {      
-            $scope.review.placeDetail = placeDetail;      
+          .then(function(placeDetail) {
+            $scope.review.placeDetail = placeDetail;
 
             $scope.map = {
               center: {
@@ -48,19 +49,15 @@ angular.module('whattoexpectatApp')
               zoom: 18
             };
 
-            $scope.placemarker = {          
+            $scope.placemarker = {
               location: {
                 latitude: placeDetail.geometry.location.lat(),
                 longitude: placeDetail.geometry.location.lng()
-              }      
+              }
             };
 
-            $scope.hasLoadedReviews = false;     
+            $scope.hasLoadedReviews = false;
 
-            // this code must run inside the .then() of a promise
-            // which retrieves the place from the database
-            // which means we need a place service that isn't
-            // our search service
             ReviewService.findReviewsByPlaceId(1)
               .then(function(reviews) {
                 $scope.hasLoadedReviews = true;
@@ -71,40 +68,42 @@ angular.module('whattoexpectatApp')
           }, function(error) {
             $location.url('/?q=' + slug.split('-').join(' '));
             return;
-          }); 
-      }
-
-      $scope.openNewReviewModal = function () {
-        if (!AuthService.user) {
-          $rootScope.openAuthModal().result.then(function(user) {
-            console.log('Auth Modal promise returned:');
-            console.log(user);
-            $scope.openNewReviewModal();
           });
-          return;
-        }
-        var modalInstance = $modal.open({
-          templateUrl: 'views/review-form.html',
-          controller: 'ReviewModalCtrl',
-          resolve: {
-            placeDetail: function() {
-              return $scope.review.placeDetail;
-            },
-            place: function() {
-              return $scope.review.place;
+
+        $scope.openNewReviewModal = function() {
+          if (!AuthService.user) {
+            $rootScope.openAuthModal().result.then(function(user) {
+              console.log('Auth Modal promise returned:');
+              console.log(user);
+              $scope.openNewReviewModal();
+            });
+            return;
+          }
+          var modalInstance = $modal.open({
+            templateUrl: 'views/review-form.html',
+            controller: 'ReviewModalCtrl',
+            resolve: {
+              placeDetail: function() {
+                return $scope.review.placeDetail;
+              },
+              place: function() {
+                return $scope.review.place;
+              },
+              gref: function() {
+                return gref;
+              }
             }
-          }             
-        });
-      };
+          });
+        };
+      }
 
       $scope.backToSearchResults = function() {
         $window.history.back();
       };
     }
   ])
-  .controller('ReviewModalCtrl', ['$scope', '$modalInstance', 'ReviewService', 'AuthService', 'place', 'placeDetail',
-    function($scope, reviewModal, ReviewService, AuthService, place, placeDetail) {
-      console.log(placeDetail);
+  .controller('ReviewModalCtrl', ['$scope', '$modalInstance', 'ReviewService', 'AuthService', 'PlaceService', 'place', 'placeDetail',
+    function($scope, reviewModal, ReviewService, AuthService, PlaceService, place, placeDetail, gref) {
       $scope.placeDetail = placeDetail;
       $scope.review = {
         text: ''
@@ -113,21 +112,34 @@ angular.module('whattoexpectatApp')
       $scope.form = {
         reviewForm: null,
         enabled: true
-      }
+      };
 
       $scope.submitReview = function() {
         $scope.form.enabled = false;
+
         var reviewText = $scope.review.text.substring(0, 140); //doubly enforce 140char max
         var recommendationModifier = $scope.review.recommendationModifier;
         var userId = AuthService.user.id;
-        var placeId = 1;
-       
-        ReviewService.addReview(placeId, userId, reviewText, recommendationModifier)
-          .then(function(review){
-            console.log('New review promise returned.');
-            console.log(review);
-            $scope.form.enabled = true;
+
+        
+        PlaceService.updatePlace({
+          slug: placeDetail.slug,
+          name: placeDetail.name,
+          gref: gref
+        }, recommendationModifier).then(function() {
+            ReviewService.addReview(placeId, userId, reviewText, recommendationModifier)
+              .then(function(review) {
+                console.log('New review promise returned.');
+                console.log(review);
+                $scope.form.enabled = true;
+              });
+          };
           });
+
+        if (!$scope.review.place.gref) {
+          // This place isn't saved yet, so let's initialize it
+          PlaceService.addPlace(placeDetail.slug, gref);
+        }
       };
     }
   ]);
---
+++ app/scripts/services/statistics.js
@@ -0,0 +1,18 @@
+'use strict';
+
+angular.module('whattoexpectatApp')
+  .factory('StatisticsService', [
+    function() {
+      return {
+        ConfidenceIntervalLowerBound(ratingTypeCount, totalRatingCount) {
+          if (totalRatingCount == 0) {
+            return 0;
+          }
+
+          var z = 1.96; // translates to 95% confidence (pnormaldist(1-(1-0.95)/2)
+          var phat = 1.0 * pos / n;
+          return (phat + z * z / (2 * n) - z * Math.sqrt((phat * (1 - phat) + z * z / (4 *n )) /n)) / (1 + z * z / n);
+        }
+      };
+    }
+  ]);
